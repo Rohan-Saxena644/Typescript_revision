ae87d80ab4bc1ab86157b9ae326fd4b8a3844dc5
Rohan-Saxena644 <rohsax83@gmail.com>
2026-04-14 21:46:31 +0530

    Objects

---
+++ src/Object.ts
@@ -0,0 +1,118 @@
+const Chai = {
+    name: "Masala Chai",
+    price: 20,
+    isHot: true
+}
+
+
+let tea: {
+    name: string;
+    price: number;
+    isHot: boolean;
+}
+
+tea ={
+    name: "Ginger Tea",
+    price: 25,
+    isHot: true
+}
+
+type Tea = {
+    name: string;
+    price: number;
+    ingredients: string[] 
+}
+
+const adrakChai: Tea ={
+    name: "Adrak Chai",
+    price: 25,
+    ingredients: ["ginger","tea leaves"]
+}
+
+type Cup = {size: string};
+let smallCup: Cup = {size: "200ml"}
+
+let bigCup = {size: "500ml" , materials: "steel"}
+
+smallCup = bigCup
+
+type Brew = {brewTime: number}
+const coffee = {brewTime: 5,beans: "Arabica"}
+const chaiBrew: Brew = coffee
+
+type User = {
+    username: string;
+    password: string
+}
+
+const u: User = {
+    username: "chaicode",
+    password: "123"
+}
+
+type Item = {name: string, quantity: number}
+type Address = {street: string, pin: number}
+
+type Order = {
+    id: string;
+    items: Item[];
+    address: Address
+}
+
+
+// data type ek tarike se define karte hain fir use dusri tarike se
+// khas baa of this topic
+
+// type Chai = {
+//     name: string;
+//     price: number;
+//     isHot: boolean
+// }
+
+const updateChai = (updates: Partial<Chai>) =>{
+    console.log("updating chai with",updates);
+}
+
+updateChai({price:25})
+updateChai({isHot: false})
+updateChai({})
+
+type ChaiOrder = {
+    name? : string
+    quantity? : number
+}
+
+const placeOrder = (order: Required<ChaiOrder>) =>{
+    console.log(order);
+}
+
+placeOrder({
+    name: "Masala Chai",
+    quantity: 2
+})
+
+
+// ye wali thing kam use hoti hai
+
+type Chai = {
+    name: string;
+    price: number;
+    isHot: boolean;
+    ingredients: string[]
+}
+
+type BasicChaiInfo = Pick<Chai,"name" | "price">;
+
+const chaiInfo: BasicChaiInfo = {
+    name: "Lemon Tea",
+    price: 30
+}
+
+type ChaiNew = {
+    name: string;
+    price: number;
+    isHot: boolean;
+    secvretingredients: string;
+};
+
+type PublicChai = Omit<Chai,"secretIngredients">;
